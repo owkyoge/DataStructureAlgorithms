49d558114b81c09f7a4ef5ae2ec806aaf6fb2bdb
unknown <owkyoge@gmail.com>
2023-04-10 06:19:14 +0530

    linkedList

--- src/ds/linkedlist/CustomLinkedList.java
+++ src/ds/linkedlist/CustomLinkedList.java
@@ -1,48 +1,65 @@
 package ds.linkedlist;
 
 public class CustomLinkedList {
-	
+
 	Node head;
 	Node tail;
-	
-	
+
 	public void add(int data) {
-		
+
 		Node newnode = new Node(data);
-		
-		if(head == null) {
+
+		if (head == null) {
 			head = newnode;
 			tail = newnode;
-			
-		}else {
+
+		} else {
 			tail.next = newnode;
 			tail = newnode;
 		}
 	}
-	
-	
+
 	public void print() {
 		Node curNode = head;
-		while(curNode != null) {
+		while (curNode != null) {
 			System.out.println(curNode.data);
-			
+
 			curNode = curNode.next;
-			
-			
+
 		}
 	}
-	
+
 	public void findNthNode(int index) {
 		int i = 1;
-		
+
 		Node curNode = head;
-		while(i!=index) {
+		while (i != index) {
 			curNode = curNode.next;
 			i++;
 		}
-		
+
 		System.out.println("Current Node DATA " + curNode.data);
-		
+
+	}
+
+	public void searchElement(int new_data) {
+		Node new_node = new Node(new_data);
+		new_node.next = head;
+		head = new_node;
+
+	}
+
+	public boolean search(Node head, int x) {
+		Node current = head;
+		while (current != null) {
+			if (current.data == x)
+				return true;
+			current = current.next;
+
+		}
+
+		return false;
+
 	}
 
 }
--- src/ds/linkedlist/TesterCL.java
+++ src/ds/linkedlist/TesterCL.java
@@ -5,20 +5,27 @@ public class TesterCL {
 	public static void main(String[] args) {
 
 		CustomLinkedList cll = new CustomLinkedList();
-		
+	
+
 		cll.add(1);
 		cll.add(2);
 		cll.add(3);
 		cll.add(4);
 		cll.add(5);
 		cll.add(6);
-		
+
 		System.out.println(cll);
 
-		
 		cll.print();
 		cll.findNthNode(5);
-		
+
+		//int x = 5;
+		int x = 15;
+		if (cll.search(cll.head, x))
+			System.out.println("YES");
+		else
+			System.out.println("No");
+
 	}
 
 }
